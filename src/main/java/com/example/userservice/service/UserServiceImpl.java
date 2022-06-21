150b3fc9e0822359b4cd701362374cc3e154b45f
CHO CHAN HEE <cksgml4424@gmail.com>
2022-06-21 14:12:59 +0900

    user spring security 적용

--- src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -7,6 +7,8 @@ import com.example.userservice.vo.ResponseOrder;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -20,11 +22,25 @@ public class UserServiceImpl implements UserService {
 	UserRepository userRepository;
 	BCryptPasswordEncoder passwordEncoder;
 
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		UserEntity userEntity = userRepository.findByEmail(username);
+
+		if (userEntity == null) {
+			throw new UsernameNotFoundException(username);
+		}
+		return new User(userEntity.getEmail(), userEntity.getEncryptedPwd(),
+				true, true, true, true,
+				new ArrayList<>());
+	}
+
 	@Autowired
 	public UserServiceImpl(UserRepository userRepository,
 						   BCryptPasswordEncoder passwordEncoder) {
 		this.userRepository = userRepository;
 		this.passwordEncoder = passwordEncoder;
+
+
 	}
 
 	@Override
@@ -41,7 +57,7 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	public UserDto getUserById(String userId) {
+	public UserDto getUserByUserId(String userId) {
 		UserEntity userEntity = userRepository.findByUserId(userId);
 
 		if (userEntity == null) {
